ec74bb48f95166f89b52dc6f0215ae9c4214e0fb
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2020-11-26 09:05:26 +0900

    Include capypara to rspec and Modify the rspec test

--- src/spec/requests/static_pages_request_spec.rb
+++ src/spec/requests/static_pages_request_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe 'StaticPages', type: :request do
   let(:base_title) { 'Snowboard App' }
 
-  describe 'GET /' do
+  describe 'visit /' do
     it 'render new' do
-      get '/'
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to match(/<title>ホーム | #{base_title}<\/title>/i)
+      visit '/'
+      expect(page).to have_title `Home | #{base_title}`
     end
   end
 end
