379621856f64f22efe0795e812321cab67dcdebb
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2020-12-06 18:10:25 +0900

    Create dropdown user menu and specs

--- src/app/controllers/sessions_controller.rb
+++ src/app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'ログイン情報が正しくありません。'
       render 'new'
--- src/spec/system/sessions_system_spec.rb
+++ src/spec/system/sessions_system_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Sessions system spec', type: :system do
   subject { page }
 
+  let(:user) { create(:user) }
+
+  describe 'Before login' do
+    before { visit '/' }
+
+    it { is_expected.to have_link 'ログイン' }
+  end
+
   describe 'Login' do
     context 'login with invalid information' do
       before do
@@ -17,6 +25,29 @@ RSpec.describe 'Sessions system spec', type: :system do
       it do
         visit '/'
         expect(page).not_to have_selector '.alert-danger'
+      end
+    end
+
+    context 'login with valid email/invalid password' do
+      before do
+        visit '/login'
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: ''
+        click_button 'ログイン'
+      end
+
+      it { is_expected.to have_selector '.alert-danger' }
+    end
+
+    context 'login with test user' do
+      before { login(user) }
+      it { is_expected.to have_current_path user_path(User.last) }
+      it { is_expected.not_to have_link login_path }
+
+      describe 'dropdown user menu' do
+        before { find_by_id('user-menu').click }
+        it { is_expected.to have_link 'ログアウト' }
+        it { is_expected.to have_link 'プロフィール' }
       end
     end
   end
