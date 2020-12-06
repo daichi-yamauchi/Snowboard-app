dce548f60d3544de41b5dbd28d1a8737a514600f
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2020-12-06 09:25:09 +0900

    Introduce system spec and Modify some specs

--- src/spec/requests/users_request_spec.rb
+++ src/spec/requests/users_request_spec.rb
@@ -1,35 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Users request', type: :request do
   describe 'GET /signup' do
     let(:base_title) { 'Snowboard App' }
 
-    it 'タイトルが表示されること' do
-      visit '/signup'
-      expect(page).to have_title "新規登録 | #{base_title}"
-    end
+    before { visit '/signup' }
+
+    it { expect(page).to have_title "新規登録 | #{base_title}" }
   end
 
-  describe 'POST'
-  context '無効なユーザ情報の場合、' do
-    # 無効なユーザ情報
-    let(:invalid_user_info) do
-      { user: { name: '',
-               email: 'user@invalid',
-               password: 'foo',
-               Password_confirmation: 'bar' } }
+  describe 'POST users_path' do
+    context 'Invalid user information post' do
+      # 無効なユーザ情報
+      let(:invalid_user_info) do
+        { user: { name: '',
+                 email: '',
+                 password: '',
+                 Password_confirmation: '' } }
+      end
+
+      it do
+        expect { post users_path, params: invalid_user_info }.to change(User, :count).by(0)
+      end
     end
 
-    it 'ユーザ登録されないこと' do
-      expect { post users_path, params: invalid_user_info }.to change(User, :count).by(0)
-    end
-
-    it 'エラーメッセージが表示されること' do
-      post users_path, params: invalid_user_info
-      expect(response.body).to include 'alert-danger'
-    end
-
-    context '有効なユーザ情報の場合、' do
+    context 'Valid user information post' do
       let(:valid_user_info) do
         { user: { name: 'Example User',
                  email: 'user@example.com',
@@ -37,13 +32,8 @@ RSpec.describe 'Users', type: :request do
                  password_confirmation: 'password' } }
       end
 
-      it 'ユーザ登録されること' do
+      it do
         expect { post users_path, params: valid_user_info }.to change(User, :count).by(1)
-      end
-
-      it 'プロフィール画面へリダイレクトされること' do
-        post users_path, params: valid_user_info
-        expect(response.body).to redirect_to user_path(User.last)
       end
     end
   end
---
+++ src/spec/system/users_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'User system spec', type: :system do
+  describe 'Signup' do
+    context 'Input valid data to form and click signup' do
+      subject { page }
+
+      before do
+        visit '/signup'
+        fill_in 'ユーザーネーム', with: 'テストユーザー'
+        fill_in 'メールアドレス', with: 'testuser@example.com'
+        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード再入力', with: 'password'
+        click_button '登録'
+      end
+
+      it { is_expected.to have_current_path user_path(User.last) }
+      it { is_expected.to have_selector '.alert-success' }
+    end
+
+    context 'Input invalid data to form and click signup' do
+      subject { page }
+
+      before do
+        visit '/signup'
+        fill_in 'ユーザーネーム', with: ''
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: ''
+        fill_in 'パスワード再入力', with: ''
+        click_button '登録'
+      end
+
+      it { is_expected.to have_current_path users_path }
+      it { is_expected.to have_selector '#error_explanation' }
+      it { is_expected.to have_selector '.alert-danger' }
+
+      it do
+        expect(all(:css, '#error_explanation li').size).to eq 5
+      end
+    end
+  end
+end
