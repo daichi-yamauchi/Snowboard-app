fac2f77aeaf0e5a9f65a53838ec8635f4a9cdc54
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2020-12-29 09:52:08 +0900

    Create display Post

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
 
   # 引数で与えられたユーザーのGravatar画像を返す
-  def gravatar_for(user)
+  def gravatar_for(user, add_class: '')
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "rounded-circle #{add_class}")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,11 @@ User.create!(name: 'Example User',
                activated: true,
                activated_at: Time.zone.now)
 end
+
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  title = Faker::Lorem.sentence(word_count: 20)
+  content = Faker::Lorem.sentence(word_count: 200)
+  users.each { |user| user.posts.create!(title: title, content: content) }
+end
