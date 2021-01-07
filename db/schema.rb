77832938f3c761d30b9e15017f92e65f84ff90c9
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2021-01-07 21:18:10 +0900

    Feat: Create relationship model for follow function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_001456) do
+ActiveRecord::Schema.define(version: 2021_01_07_114713) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_01_07_001456) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "name_j"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "post_types", force: :cascade do |t|
     t.string "name"
     t.string "name_j"
@@ -50,6 +57,43 @@ ActiveRecord::Schema.define(version: 2021_01_07_001456) do
     t.index ["post_type_id"], name: "index_posts_on_post_type_id"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -71,4 +115,5 @@ ActiveRecord::Schema.define(version: 2021_01_07_001456) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "post_types"
   add_foreign_key "posts", "users"
+  add_foreign_key "taggings", "tags"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { user }
-  let(:user) { build(:user) }
-
-  it { is_expected.to be_valid }
-
   describe 'Attribute' do
+    describe 'Valid user' do
+      let(:user) { build(:user) }
+      it { expect(user).to be_valid }
+    end
+
     describe 'User name' do
       context 'Empty user name' do
-        before { user.name = ' ' }
-        it { is_expected.not_to be_valid }
+        let(:user) { build(:user, name: ' ') }
+        it { expect(user).not_to be_valid }
       end
 
       context 'Valid long user name' do
-        before { user.name = 'a' * 30 }
-        it { is_expected.to be_valid }
+        let(:user) { build(:user, name: 'a' * 30) }
+        it { expect(user).to be_valid }
       end
 
       context 'Too long user name' do
-        before { user.name = 'a' * 31 }
-        it { is_expected.not_to be_valid }
+        let(:user) { build(:user, name: 'a' * 31) }
+        it { expect(user).not_to be_valid }
       end
     end
 
     describe 'Email' do
       context 'Empty email address' do
-        before { user.email = ' ' }
-        it { is_expected.not_to be_valid }
+        let(:user) { build(:user, email: ' ') }
+        it { expect(user).not_to be_valid }
       end
 
       context 'Valid long email address' do
-        before { user.email = "#{'a' * 243}@example.com" }
-        it { is_expected.to be_valid }
+        let(:user) { build(:user, email: "#{'a' * 243}@example.com") }
+        it { expect(user).to be_valid }
       end
 
       context 'Too long email address' do
-        before { user.email = "#{'a' * 244}@example.com" }
-        it { is_expected.not_to be_valid }
+        let(:user) { build(:user, email: "#{'a' * 244}@example.com") }
+        it { expect(user).not_to be_valid }
       end
 
       context 'Valid email address' do
@@ -46,7 +46,7 @@ RSpec.describe User, type: :model do
         end
         it do
           valid_emails.each do |email|
-            user.email = email
+            user = build(:user, email: email)
             expect(user).to be_valid
           end
         end
@@ -58,23 +58,21 @@ RSpec.describe User, type: :model do
         end
         it do
           invalid_emails.each do |email|
-            user.email = email
+            user = build(:user, email: email)
             expect(user).not_to be_valid
           end
         end
       end
 
       context 'Same email address' do
+        let(:user) { build(:user) }
         before { create(:user, email: user.email.upcase) }
-        it { is_expected.not_to be_valid }
+        it { expect(user).not_to be_valid }
       end
 
       context 'Mixed case email' do
         let(:mixed_case_email) { 'Foo@ExAMPle.CoM' }
-        before do
-          user.email = mixed_case_email
-          user.save
-        end
+        let(:user) { create(:user, email: mixed_case_email) }
         it { expect(user.email).to eq mixed_case_email.downcase }
       end
     end
@@ -82,16 +80,50 @@ RSpec.describe User, type: :model do
     describe 'Password' do
       context 'Too short password' do
         let(:pw) { 'a' * 7 }
-        before { user.password = user.password_confirmation = pw }
-        it { is_expected.not_to be_valid }
+        let(:user) { build(:user, password: pw, password_confirmation: pw) }
+        it { expect(user).not_to be_valid }
       end
     end
   end
 
   describe 'Instance methods' do
+    let(:user) { create(:user) }
+    let(:followed) { create(:user) }
+    let(:unfollowed) { create(:user) }
+    before { user.follow(followed) }
+
     describe 'def authenticated?(remember_token)' do
       context 'remember_digest is nil' do
         it { user.authenticated?(:remember, '') }
+      end
+    end
+
+    describe 'def follow and unfollow' do
+      it 'follow make following? true' do
+        expect(user.following?(followed)).to be true
+        expect(followed.followers.include?(user)).to be true
+      end
+
+      it 'unfollow make following? false' do
+        user.unfollow(followed)
+        expect(user.following?(followed)).to be false
+      end
+    end
+
+    xdescribe 'def feed' do
+      before do
+        create_list(:post, 10, user: user)
+        create_list(:post, 10, user: followed)
+        create_list(:post, 10, user: unfollowed)
+      end
+      it "is exected to include followed's posts" do
+        followed.posts.each { |post| expect(user.feed).to include post }
+      end
+      it 'is exected to include my posts' do
+        user.posts.each { |post| expect(user.feed).to include post }
+      end
+      it "is exected not to include unfollowed's posts" do
+        unfollowed.posts.each { |post| expect(user.feed).not_to include post }
       end
     end
   end
@@ -108,8 +140,8 @@ RSpec.describe User, type: :model do
 
   describe 'Associate' do
     describe 'post dependent on user for destroy' do
+      let(:user) { create(:user) }
       before do
-        user.save
         create(:post, user: user)
       end
       it 'is expected to destroy when user is destroyed' do
